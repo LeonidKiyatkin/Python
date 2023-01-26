71f982d780f0596b70adc9315f952e85ee9d6ec8
Leonid <leonid@mail.ru>
2023-01-26 22:22:30 +0300

    first commit

---
+++ Desktop/python/hw/1/1.py
@@ -0,0 +1,5 @@
+name_user = input('Введите ваше имя - ')
+user_password = input('Введите ваш пароль - ')
+user_age = int(input('Введите ваш возраст - '))
+print(f'Ваши данные для входа в аккаунт: имя - {name_user}, '
+      f'пароль - {user_password}, возраст - {user_age}')
